5628b4e0ea941293e39d6beb38bdc39f818ae7b9
Javier Riveros <javiiriveros@hotmail.com>
2018-11-06 23:35:39 -0500

    Added user auth with Google

---
+++ src/Login.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import firebase from './initializers/firebase';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/photoslibrary.readonly");
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        let accessToken = result.credential.accessToken;
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button variant="extendedFab" color="secondary" aria-label="Login">
+          Login with Google
+        </Button>
+      </div>
+    );
+  }
+}
